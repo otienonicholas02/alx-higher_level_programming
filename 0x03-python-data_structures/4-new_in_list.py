5a38f940813b53a0bcde5afb33369be7995a2f97
Nicholas Otieno <otienonicholas02@gmail.com>
2023-05-15 08:54:12 -0700

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list.copy())
+    new_copy = my_list.copy()
+    new_copy[idx] = element
+    return (new_copy)
