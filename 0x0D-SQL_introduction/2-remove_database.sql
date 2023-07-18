572fe4402c2b294b6cbbcd375110ab8b6268e7ab
Nicholas Otieno <otienonicholas02@gmail.com>
2023-07-18 13:31:02 -0700

    update

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
---deletes the database hbtn_0c_0 in your MySQL server
-DROP DATABASE IF EXISTS hbtn_0c_0;
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
