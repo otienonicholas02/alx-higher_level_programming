08e21dd00b7a16133942ed045e954c9c0397cafe
Nicholas Otieno <otienonicholas02@gmail.com>
2023-10-04 21:24:20 +0300

    1-script.js

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  function translate () {
+    $('DIV#hello').empty();
+    const len = $('INPUT#language_code').val();
+    $.ajax({
+      type: 'GET',
+      url: 'https://fourtonfish.com/hellosalut/?lang=' + len,
+      success: function (data) {
+        $('DIV#hello').append(data.hello);
+      }
+    });
+  }
+  $('INPUT#btn_translate').click(function () {
+    translate();
+  });
